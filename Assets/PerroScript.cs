c4b4453b460d29a473039d3411d9eb488a6e4350
freidylcarlos <freidylcarlos@gmail.com>
2025-08-09 20:04:10 -0500

    ** Perro Idle

---
+++ Assets/PerroScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerroScript : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D rb;
+    private Animator Anim;
+    private float horizontal;
+    private bool bIsJumping = false;
+    private int Health = 5;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        //Anim.SetBool("Running", horizontal != 0.0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if (bIsJumping)
+        {
+            return;
+        }
+
+        bIsJumping = true;
+
+        rb.AddForce(Vector2.up * JumpForce);
+    }
+
+    private void EndJump()
+    {
+        bIsJumping = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            EndJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * Speed, rb.velocity.y);
+    }
+
+    public void Hit()
+    {
+        Health = Health - 1;
+        if (Health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
